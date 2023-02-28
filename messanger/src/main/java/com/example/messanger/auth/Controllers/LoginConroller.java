b2ab2baddb947d9b379a15f4f0da61f39d205f58
Georgii <panovgeorgij819@gmail.com>
2023-02-28 07:08:21 +0500

    add LoginConroller

---
+++ messanger/src/main/java/com/example/messanger/auth/Controllers/LoginConroller.java
@@ -0,0 +1,133 @@
+package com.example.messanger.auth.Controllers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.messanger.auth.User.UserRepo;
+import com.example.messanger.auth.forms.LoginForm;
+import com.example.messanger.auth.forms.RegistrationForm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Controller
+@RequestMapping
+@AllArgsConstructor
+public class LoginConroller {
+    public UserRepo userRepository;
+
+    @GetMapping("login")
+    public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
+        var cookies = request.getCookies();
+
+        String token = null;
+
+        if (cookies == null) {
+            return "login";
+        }
+
+        else {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("auth_token")) {
+                    token = cookie.getValue();
+                }
+            }
+        }
+
+        try {
+            if (token != null) {
+                return "redirect:/";
+            }
+        }
+
+        catch (NullPointerException exception) {
+            model.addAttribute("error_login", "Ошибка в логин");
+        }
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(LoginForm form, Model model, RegistrationForm registrationForm, HttpServletResponse response, HttpServletRequest request) {
+        var cookies = request.getCookies();
+        String token = null;
+
+        if (cookies == null) {
+            if (Objects.equals(form.getLogin(), "")) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        else {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("auth_token")) {
+                    token = cookie.getValue();
+                }
+            }
+
+            try {
+                if (token != null) {
+                    response.setStatus(HttpServletResponse.SC_SEE_OTHER);
+                    return "redirect:/";
+                }
+
+                else {
+                    String secret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE";
+
+                    try {
+                        if (form.getLogin().length() > 8 && form.getPassword().length() > 8) {
+                            if (userRepository.validPassword(form.getLogin(), form.getPassword())) {
+                                System.out.println("PASSWORD IS CORRECT");
+                                model.addAttribute("username", userRepository.select_username(registrationForm));
+                                try {
+                                    Algorithm algorithm = Algorithm.HMAC512(secret);
+                                    String jwtToken = JWT.create()
+                                            .withIssuer(form.getLogin())
+                                            .withClaim("userId", "1234")
+                                            .withSubject(form.getLogin())
+                                            .withIssuedAt(new Date())
+                                            .withExpiresAt(new Date(System.currentTimeMillis() + 5000L))
+                                            .sign(algorithm);
+                                    JWTVerifier verifier = JWT.require(Algorithm.HMAC512(secret))
+                                            .build();
+                                    DecodedJWT decodedJWT = verifier.verify(jwtToken);
+                                    System.out.println(decodedJWT);
+                                    Cookie cookie = new Cookie("auth_token", jwtToken);
+                                    response.addCookie(cookie);
+                                } catch (JWTCreationException | org.springframework.dao.EmptyResultDataAccessException |
+                                         org.springframework.dao.DataIntegrityViolationException exception) {
+                                    System.out.println("Ошибка в создании JWT token");
+                                }
+                                return "redirect:/";
+                            }
+                        }
+                    }
+
+                    catch (org.springframework.dao.EmptyResultDataAccessException | org.springframework.dao.DataIntegrityViolationException exception) {
+                        System.out.println("Такого пользователя не существует");
+                        model.addAttribute("not_valid_user", "Такого пользователя не существует");
+                        return "login";
+                    }
+                }
+            }
+
+            catch (NullPointerException exception) {
+                model.addAttribute("error_login", "Ошибка в логине");
+            }
+        }
+
+        return "redirect:/login";
+    }
+}
