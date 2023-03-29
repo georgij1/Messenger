44f81d3ce070c355f70f3c7d7f4445b7dd799d2e
Georgii <panovgeorgij819@gmail.com>
2023-03-29 22:17:06 +0500

    update sql script in v3.5 social network

--- messanger/src/main/resources/db/migration/V1__init.sql
+++ messanger/src/main/resources/db/migration/V1__init.sql
@@ -1,43 +1,36 @@
 create table public.users
 (
-    id            serial primary key,
-    username      text not null unique,
+    id serial primary key,
+    username text not null unique,
     password_hash text not null,
-    image         text default '../image/settings/icon_profile.png'::text
+    image text default '../image/settings/icon_profile.png'::text
 );
 
 create table public.chat
 (
-    id   serial primary key,
-    name text not null
+    id serial primary key,
+    name text,
+    desc_chat text
 );
 
 create table public.message
 (
-    id         serial primary key,
-    text       text,
-    sender_id  integer not null references public.users (id),
-    chat_id    integer not null references public.chat (id),
-    time_stamp timestamp default NOW()
+    id serial primary key,
+    text text,
+    sender_id integer not null references public.users,
+    chat_id integer not null references public.chat,
+    time_stamp text
 );
-
--- create table public.users
--- (
---     id serial primary key,
---     username text not null unique,
---     password_hash text not null,
---     image text default '../image/settings/icon_profile.png'::text
--- );
 
 create table public.role
 (
-    id   serial primary key,
+    id serial primary key,
     name text
 );
 
 create table public.userchat
 (
-    user_id integer references public.users (id),
-    chat_id integer references public.chat (id),
-    role_id integer not null references public.role (id)
+    user_id integer references public.users,
+    chat_id integer references public.chat,
+    role_id integer not null references public.role
 );
