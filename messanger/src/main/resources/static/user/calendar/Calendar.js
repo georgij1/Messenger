113549a8db1cce8a13e9253ad795a3d461cfc5cf
Georgii <panovgeorgij819@gmail.com>
2023-04-16 00:54:23 +0500

    fix bug in style in calendar

---
+++ messanger/src/main/resources/static/user/calendar/Calendar.js
@@ -0,0 +1,88 @@
+let btn_up = document.querySelector('.btn_up')
+let delete_account = document.querySelector('.delete_account')
+let min_size_window = document.querySelectorAll('.min_size_window')
+let header = document.querySelectorAll('.header')
+let body_class = document.querySelectorAll('.body')
+let flex_content = document.querySelectorAll('.flex-content')
+let burger_menu = document.querySelectorAll('.burger_menu')
+let flex = document.querySelector('.flex')
+let body = document.querySelector('.body')
+
+fetch('/image_profile',{
+    headers: new Headers({
+        'Content-Type': 'application/json'
+    }),
+    mode: "cors"
+})
+    .then(response => response.json())
+    .then((data) => (data.forEach((item) => {
+        console.log(item.image)
+        let image_profile = document.querySelectorAll('.image_profile')
+        for (let image_profile_1 of image_profile) {
+            image_profile_1.style.background=`url(${item.image})` + 'no-repeat'
+            image_profile_1.style.backgroundSize=`100%`
+            image_profile_1.style.width=`34%`
+            image_profile_1.style.borderRadius=`50px`
+        }
+    })));
+
+
+btn_up.addEventListener('click', () => {
+    btn_up.classList.toggle('round')
+    delete_account.classList.toggle('block')
+})
+
+for (let min_size_windo of min_size_window) {
+    min_size_windo.addEventListener('click', () => {
+        for (let head of header) {
+            min_size_windo.classList.toggle('none')
+            head.classList.toggle('visible')
+            for (let body of body_class) {
+                body.classList.remove('max_size_window')
+            }
+            for (let menu of burger_menu) {
+                menu.classList.remove('none')
+            }
+            for (let contents of document.querySelectorAll('.content')) {
+                contents.classList.remove('none_content')
+            }
+            for (let flex_content_itter of flex_content) {
+                flex_content_itter.classList.toggle('flex-content-new')
+                flex_content_itter.classList.remove('flex-content-new-burger-open')
+            }
+            let chat = document.querySelectorAll('.chat')
+            for (let chat_all of chat) {
+                chat_all.classList.remove('chat_burger_open')
+            }
+            body.classList.remove('None768px')
+        }
+    })
+}
+
+
+for (let menu of burger_menu) {
+    menu.addEventListener('click', () => {
+        for (let head of header) {
+            head.classList.toggle('visible')
+            menu.classList.toggle('none')
+            for (let min_size_windows of min_size_window) {
+                min_size_windows.classList.remove('none')
+            }
+            for (let body of body_class) {
+                body.classList.toggle('max_size_window')
+            }
+            for (let contents of document.querySelectorAll('.content')) {
+                contents.classList.toggle('none_content')
+            }
+            for (let flex_content_itter of flex_content) {
+                flex_content_itter.classList.toggle('flex-content-new')
+                flex_content_itter.classList.toggle('flex-content-new-burger-open')
+            }
+            let chat = document.querySelectorAll('.chat')
+            for (let chat_all of chat) {
+                chat_all.classList.toggle('chat_burger_open')
+            }
+            body.classList.add('None768px')
+        }
+    })
+}
