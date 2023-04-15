f0f03f523586b54c4e41e9a5cb9aabc92d9e2acd
Georgii <panovgeorgij819@gmail.com>
2023-04-15 14:36:25 +0500

    update style in rings

--- messanger/src/main/resources/static/user/rings/script.js
+++ messanger/src/main/resources/static/user/rings/script.js
@@ -3,10 +3,14 @@ let BtnGetSendAccessOffer = document.querySelector('.BtnGetSendAccessOffer')
 let BtnGetSendAccessOfferCheck = document.querySelector('.BtnGetSendAccessOfferCheck')
 let MessageMessanger = document.querySelector('.MessageMessanger')
 let EventCalendar = document.querySelector('.EventCalendar')
-let ContentRequestChat = document.querySelector('.ContentRequestChat')
+let ContentAllRequestChat = document.querySelector('.ContentAllRequestChat')
 let CloseWindowRequest = document.querySelector('.CloseWindowRequest')
 let AllRequest = document.querySelector('.AllRequest')
 let BtnHeader = document.querySelector('.BtnHeader')
+let ContentSentRequestChat = document.querySelector('.ContentSentRequestChat')
+let ContentCheckRequestChat = document.querySelector('.ContentCheckRequestChat')
+let ContentMessageFromChat = document.querySelector('.ContentMessageFromChat')
+let ContentEventMessage = document.querySelector('.ContentEventMessage')
 
 RequestChat.addEventListener('click', () => {
     BtnGetSendAccessOfferCheck.classList.add('block')
@@ -16,6 +20,10 @@ RequestChat.addEventListener('click', () => {
     EventCalendar.classList.add('none')
     CloseWindowRequest.classList.add('block')
     AllRequest.classList.add('block')
+    ContentAllRequestChat.classList.add('block')
+    ContentSentRequestChat.classList.remove('block')
+    ContentCheckRequestChat.classList.remove('block')
+    ContentMessageFromChat.classList.remove('block')
 })
 
 CloseWindowRequest.addEventListener('click', () => {
@@ -27,14 +35,39 @@ CloseWindowRequest.addEventListener('click', () => {
     CloseWindowRequest.classList.remove('block')
     AllRequest.classList.remove('block')
     BtnHeader.classList.remove('none')
+    ContentAllRequestChat.classList.remove('block')
+    ContentSentRequestChat.classList.remove('block')
+    ContentCheckRequestChat.classList.remove('block')
+    ContentMessageFromChat.classList.remove('block')
+    ContentEventMessage.classList.remove('block')
 })
 
 MessageMessanger.addEventListener('click', () => {
     BtnHeader.classList.add('none')
     CloseWindowRequest.classList.add('block')
+    ContentMessageFromChat.classList.add('block')
 })
 
 EventCalendar.addEventListener('click', () => {
     BtnHeader.classList.add('none')
     CloseWindowRequest.classList.add('block')
+    ContentEventMessage.classList.add('block')
+})
+
+AllRequest.addEventListener('click', () => {
+    ContentAllRequestChat.classList.add('block')
+    ContentSentRequestChat.classList.remove('block')
+    ContentCheckRequestChat.classList.remove('block')
+})
+
+BtnGetSendAccessOffer.addEventListener('click', () => {
+    ContentAllRequestChat.classList.remove('block')
+    ContentSentRequestChat.classList.add('block')
+    ContentCheckRequestChat.classList.remove('block')
+})
+
+BtnGetSendAccessOfferCheck.addEventListener('click', () => {
+    ContentAllRequestChat.classList.remove('block')
+    ContentSentRequestChat.classList.remove('block')
+    ContentCheckRequestChat.classList.add('block')
 })
