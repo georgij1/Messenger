0f8ffacec0ea5cd451be4e302aa6982143c29457
Georgii <panovgeorgij819@gmail.com>
2023-04-16 23:18:28 +0500

    update sql scripts

--- messanger/src/main/resources/db/migration/V1__init.sql
+++ messanger/src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,4 @@
-create table public.users
+create table users
 (
     id serial primary key,
     username text not null unique,
@@ -6,29 +6,30 @@ create table public.users
     image text default '../image/settings/icon_profile.png'
 );
 
-create table public.chat
+create table chat
 (
     id serial primary key,
     name text unique,
-    desc_chat text default 'Описания нет',
+    desc_chat  text default 'Описания нет',
     type text,
     image_chat text default '../image/settings/icon_profile.png',
     owner text not null
 );
 
-create table public.message
+create table message
 (
     id serial primary key,
     text text,
-    sender_id integer not null references public.users,
-    chat_id integer not null references public.chat,
-    time_stamp text
+    sender_id integer not null references users,
+    chat_id integer not null references chat,
+    time_stamp_short text,
+    time_stamp_long  text
 );
 
-create table public.users_chat
+create table users_chat
 (
-    id integer not null primary key,
-    name text,
+    name       text,
     chat_nane text,
-    image_user text default '../image/settings/icon_profile.png'
+    image_user text default '../image/settings/icon_profile.png',
+    id serial not null primary key
 );
--- messanger/src/main/resources/db/migration/V2__init.sql
+++ messanger/src/main/resources/db/migration/V2__init.sql
@@ -1,22 +1,35 @@
-create table public.mycontacts
+create table mycontacts
 (
     id serial primary key,
     username text unique,
     image text
 );
 
-create table public.image_message
+create table image_message
 (
-    id serial primary key,
+    id   text,
     name text,
     data text,
-    type text
+    type text,
+    time_stamp_short text,
+    time_stamp_long text
 );
 
-create table public.comand_dev
+create table comand_dev
 (
     id serial primary key,
     name text default 'Имя не задано',
     about_me text default 'Информации о себе нет',
     link_portfolio text default 'Ссылки на портфолио нет'
+);
+
+create table send_access_to_chat_post
+(
+    id serial not null primary key,
+    usernamefromsent text,
+    chat_name text,
+    usernametosent text,
+    access boolean,
+    order_status boolean default true,
+    cancel boolean default false
 );
